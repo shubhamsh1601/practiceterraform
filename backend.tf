357d41c7506d166b76e94124c6594540ceefe82b
Shubham <shubhamsh1601@gmail.com>
2026-02-24 02:13:57 +0530

    gg

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-/*terraform {
+terraform {
   backend "azurerm" {
     storage_account_name = "testsamonika00001"
     resource_group_name = "myrg"
     container_name = "tfstate"
     key = "prod.tfstate"
   }
-} */
+} 
